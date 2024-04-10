28b821091cf619a73fe15905aa869e1dec3aa541
ChiThienKaka <thien190602@gmail.com>
2024-04-11 00:34:22 +0700

    baitap1

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { View, Text } from 'react-native'
+import React from 'react'
+import Bt8Component from './.expo/src/Bt8Component'
+import Bt9Component from './.expo/src/Bt9Component'
+import Bt2Component from './.expo/src/Bt2Component'
+import Bt3Component from './.expo/src/Bt3Component'
+import Bt4Component from './.expo/src/Bt4Component'
+import Bt5Component from './.expo/src/Bt5Component'
+import Bt6Component from './.expo/src/Bt6Component'
+import Bt7Component from './.expo/src/Bt7Component'
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Bt7Component/>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
