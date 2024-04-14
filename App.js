330262005130286904e1531c7a177e0db7b7fcee
ChiThienKaka <thien190602@gmail.com>
2024-04-14 22:03:57 +0700

    thuchanhlap1

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Bt8Component from './.expo/src/Bt8Component'
-import Bt9Component from './.expo/src/Bt9Component'
-import Bt2Component from './.expo/src/Bt2Component'
-import Bt3Component from './.expo/src/Bt3Component'
-import Bt4Component from './.expo/src/Bt4Component'
-import Bt5Component from './.expo/src/Bt5Component'
-import Bt6Component from './.expo/src/Bt6Component'
-import Bt7Component from './.expo/src/Bt7Component'
+import Bt8Component from './Bt8Component'
+import Bt9Component from './Bt9Component'
+import Bt2Component from './Bt2Component'
+import Bt3Component from './Bt3Component'
+import Bt4Component from './Bt4Component'
+import Bt5Component from './Bt5Component'
+import Bt6Component from './Bt6Component'
+import Bt7Component from './Bt7Component'
 const App = () => {
   return (
     <Bt7Component/>
